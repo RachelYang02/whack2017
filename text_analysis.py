52486cd8296047c4b19430336206f99018099b14
Rachel Yang <rachel.yang@students.olin.edu>
2017-11-10 23:05:20 -0500

    text analysis: passive voice

--- text_analysis.py
+++ text_analysis.py
@@ -8,10 +8,10 @@ Speed, and Sentiment.
 import indicoio
 import time
 import re
+import spacy
 indicoio.config.api_key = '7b7ddd4e3120df54b1a4018d77e01f9c'
 
-f = open('pride.txt','r')
-test_paragraph = f.read()
+test_paragraph = open('pride.txt','r').read()
 
 # Sentiment
 def sentiment_analysis(string):
@@ -39,10 +39,25 @@ print time_to_m(length_analysis(test_paragraph))
 
 # Speed
 def speed_analysis(string):
-	passage = []
+	passage_word = []
 	parsed_string = re.sub(r'[^\w\s]','',string)	
-	passage = parsed_string.split(" ")
-	return length_analysis(string) / len(passage)
+	passage_word = parsed_string.split(" ")
+	return length_analysis(string) / float(len(passage_word))
 	
 print speed_analysis(test_paragraph)
 
+
+# Passive Voice
+def passive_analysis(string):
+	passive_voice = {'passive': 0}
+	passive_verb = ['is','are','am','was','were','had','has','have']
+	passage_sent = re.split('(?<=[.!?]) +',string)
+	for sent in passage_sent:
+		passage_word_sent = sent.split(" ")
+		for i, word in enumerate(passage_word_sent):
+			if word in passive_verb:
+				if (passage_word_sent[i+1][-3:] == 'ing') or (passage_word_sent[i+1][-2:] == 'en') or (passage_word_sent[i+1][-2:] == 'ed'):
+					passive_voice['passive'] += 1
+
+	return passive_voice['passive'] / float(len(passage_sent))
+print passive_analysis(test_paragraph)
