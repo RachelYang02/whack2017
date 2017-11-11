fd355f2fe3bc41a3e0261b23ffb4c070d65291f8
Rachel Yang <rachel.yang@students.olin.edu>
2017-11-11 14:11:27 -0500

    update tagger and push randomizer

--- tagger.py
+++ tagger.py
@@ -2,53 +2,62 @@
 WHACK 2017
 Input: questions.txt
 Tag questions with labels given in file.
-Output: json of Question Objects
+Output: json of Question Dictionaries
 """
 
 import re
+import json
 
-def split_question(question):
-	""" Split and return labels for each question. """
-	question_and_labels = re.split('(?<=[.!?]) +',question)
-	tags = re.sub('[(){}<>]', '', question_and_labels[1])
-	tags = tags.split(", ")
-	tags = [tag for tag in tags if tag is not None] 
-	if len(tags) == 3:
-		return (question_and_labels[0], tags[0], tags[1], tags[2])
+class Tagger():
+	""" Parse input text file of questions and create 
+		json of Question Dictionaries (question_text, labels)."""
+	def __init__(self, question_file):
+		self.question_file = question_file
 
-def main(questions_l):
-	""" Save each question and labels in dictionary, 
-		and save collection of dictionaries in list. """
-	questions_final = []
-	for i, question in enumerate(questions_l):
+	def split_question(self, question):
+		labels_difficulty = ["easy", "medium", "hard"]
+		labels_length = ["short", "medium", "long"]
+		labels_category = ["Interpersonal Skills", "Leadership", "Failure_Frustration", "Time Management_Prioritization"]
+		""" Split and return labels for each question. """
+		question_and_labels = re.split('(?<=[.!?]) +',question)
+		tags = re.sub('[(){}<>]', '', question_and_labels[1])
+		tags = tags.split(", ")
+		tags = [tag for tag in tags if tag is not None] 
+		if len(tags) == 3:
+			tags[0] = labels_difficulty.index(tags[0])
+			tags[1] = labels_length.index(tags[1])
+			tags[2] = labels_category.index(tags[2])
+			return (question_and_labels[0], tags)
+
+	def json_build(self, questions_l):
+		""" Save each question and labels in dictionary, 
+			and save collection of dictionaries in list. """
+		questions_final = {}
 		questions_dict = {}
-		split_question(question)
-		if split_question(question):
-	 		(question_text, tag_difficulty, tag_length, tag_category) = split_question(question)
-	 		questions_dict["name"] = "Question %d" % (i+1)
-	 		questions_dict["text"] = question_text
-	 		questions_dict["difficulty"] = tag_difficulty
-	 		questions_dict["length"] = tag_length
-	 		questions_dict["category"] = tag_category
-	  		questions_final.append(questions_dict)
-	return questions_final
+		for i, question in enumerate(questions_l):
+			questions_tags_dict = {}
+			self.split_question(question)
+			if self.split_question(question):
+		 		(question_text, tags) = self.split_question(question)
+		 		questions_tags_dict["text"] = question_text
+		 		questions_tags_dict["labels"] = tags
+
+		 		questions_dict["Question %d" % (i+1)] = questions_tags_dict
+		questions_final["questions"] = questions_dict
+		questions_json = json.dumps(questions_final)
+		return questions_json
 
 
-def tagger(question_file):
-	""" Read from question file,
-		parse text, and save list of
-		individual questions. """
-	q = []
-	with open(question_file, 'r') as questions_all:
-		for question in questions_all:
-			que = question.strip()
-			if que:
-				question = re.sub('[/]', '', question)
-				q.append(question.strip())
-		q = [x for x in q if x is not None]
-		return main(q)
-
-# See Results
-tagger = tagger('questions.txt')
-if tagger:
-	print tagger
+	def tagger(self):
+		""" Read from question file,
+			parse text, and save list of
+			individual questions. """
+		q = []
+		with open(self.question_file, 'r') as questions_all:
+			for question in questions_all:
+				que = question.strip()
+				if que:
+					question = re.sub('[/]', '', question)
+					q.append(question.strip())
+			q = [x for x in q if x is not None]
+			return self.json_build(q)
